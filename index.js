be956741b88d4543e4c8de9b232944de90129dcf
Enoch2k2 <enoch2k2@hotmail.com>
2021-06-11 10:21:02 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+// function createAnimal(name) {
+//   return function(superpower) {
+//     console.log(`This ${name} has this ${superpower} superpower.`)
+//   }
+// }
+
+
+// const sharkCreator = createAnimal('Shark');
+// const monkeyCreator = createAnimal('Monkey');
+
+// sharkCreator('LaserBeam Eyes');
+// sharkCreator('Super Hungry');
+// sharkCreator('Flying');
+
+// monkeyCreator('Really big');
+// monkeyCreator('Speaks Spanish');
+
